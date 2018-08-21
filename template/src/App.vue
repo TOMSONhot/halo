75046ffbee8b9a33d061433d2de56427d1b39a1c
TOMSONhot <240095970@qq.com>
2018-08-21 14:19:52 +0800

    Update App.vue

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    {{#router}}
     <router-view/>
-    {{else}}
-    <HelloWorld/>
-    {{/router}}
+    <el-select style="width: 100px;margin-left: 15px;"
+      v-model="lang"
+      placeholder="请选择"
+      @change="changeLang">
+      <el-option label="中文"
+        value="zh_CN"></el-option>
+      <el-option label="EN"
+        value="en_US"></el-option>
+    </el-select>
   </div>
 </template>
 
 <script>
-{{#unless router}}
-import HelloWorld from './components/HelloWorld'
+import i18n from '@/i18n';
+import locale from 'hui/lib/locale';
 
-{{/unless}}
 export default {
-  name: 'App'{{#router}}{{else}},
-  components: {
-    HelloWorld
-  }{{/router}}
-}
+  name: 'App',
+  data() {
+    return {
+      lang: 'zh_CN'
+    };
+  },
+  created() {},
+  methods: {
+    // 切换语言
+    changeLang() {
+      let lang;
+      if (this.lang === 'zh_CN') lang = require('@/i18n/zh_CN');
+      else lang = require('@/i18n/en_US');
+      i18n.setLocaleMessage(this.lang, JSON.parse(JSON.stringify(lang)));
+      locale.i18n((key, value) => i18n.t(key, value));
+      i18n.locale = this.lang;
+    }
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
