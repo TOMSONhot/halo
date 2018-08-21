88c89d2a7e152a496f4580b15e909bbdb2b17144
TOMSONhot <240095970@qq.com>
2018-08-21 14:18:08 +0800

    Create api-test.vue

---
+++ template/src/view/test-lab/api-test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{$t('demo.common.language')}}1231231</div>
+</template>
+<script>
+// import testLabApi from '../../api/testLab';
+import api from '../../api/api';
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    api.testLab.list({ isBigData: false }).then(res => {
+      console.log(res);
+    });
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
